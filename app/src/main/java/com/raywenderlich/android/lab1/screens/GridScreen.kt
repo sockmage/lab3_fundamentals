aff51b857ff4d6fb2226b481684f32e45fe28d86
sockmage <cockmage@icloud.com>
2024-05-13 01:34:33 +0300

    Реализация GridView

--- app/src/main/java/com/raywenderlich/android/lab1/screens/GridScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/GridScreen.kt
@@ -1,6 +1,9 @@
 package com.raywenderlich.android.lab1.screens
 
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Build
 import androidx.compose.material.icons.filled.Check
@@ -9,10 +12,12 @@ import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.ThumbUp
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import com.raywenderlich.android.lab1.router.BackButtonHandler
 import com.raywenderlich.android.lab1.router.FundamentalsRouter
 import com.raywenderlich.android.lab1.router.Screen
+import kotlin.math.ceil
 
 private val items = listOf(
     Icons.Filled.Check,
@@ -38,8 +43,29 @@ fun GridScreen() {
 
 
 @Composable
-fun GridView(columnCount: Int) {
+fun GridView(columnCount: Int) { val itemSize = items.size
+    val rowCount = ceil(itemSize.toFloat() / columnCount).toInt()
+    val gridItems = mutableListOf<List<IconResource>>()
+    var position = 0
+    for (i in 0 until rowCount) {
+        val rowItem = mutableListOf<IconResource>()
+        for (j in 0 until columnCount) {
+            if (position.inc() <= itemSize) {
+                rowItem.add(IconResource(items[position++], true))
+            }
+        }
+        val itemsToFill = columnCount - rowItem.size
 
+        for (j in 0 until itemsToFill) {
+            rowItem.add(IconResource(Icons.Filled.Delete, false))
+        }
+        gridItems.add(rowItem)
+    }
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        items(gridItems) { items ->
+            RowItem(items)
+        }
+    }
 }
 
 
