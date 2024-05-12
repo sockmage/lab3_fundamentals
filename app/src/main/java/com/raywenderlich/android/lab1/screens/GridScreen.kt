c273360ad068370ed28928555e11d741f3878993
sockmage <cockmage@icloud.com>
2024-05-13 01:38:37 +0300

    Реализация RowItem

--- app/src/main/java/com/raywenderlich/android/lab1/screens/GridScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/GridScreen.kt
@@ -1,5 +1,6 @@
 package com.raywenderlich.android.lab1.screens
 
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
@@ -71,7 +72,10 @@ fun GridView(columnCount: Int) { val itemSize = items.size
 
 @Composable
 fun RowItem(rowItems: List<IconResource>) {
-
+    Row {
+        for (element in rowItems)
+            GridIcon(element)
+    }
 }
 
 
